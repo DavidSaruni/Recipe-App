26f7b158d07d5e3f28da4a2da5f234292b501cbd
DavidSaruni <sarunidavid11126@gmail.com>
2024-08-27 02:01:27 +0300

    Navigation Component(Bottom Navigation)

---
+++ app/src/main/java/com/example/recipe/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.recipe
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Apply window insets to handle edge-to-edge layout
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Find the BottomNavigationView and NavController
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        // Set up BottomNavigationView with NavController
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+    }
+}
